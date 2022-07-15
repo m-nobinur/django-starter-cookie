9b06ac36ba1598a0e8325bee2023510bfd08ec1a
m-nobinur <mohammadnobinur@gmail.com>
2022-07-15 13:51:18 +0600

    Add storages utils

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_base}}/utils/storages.py
@@ -0,0 +1,26 @@
+{% if cookiecutter.cloud_provider == 'AWS' -%}
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticRootS3Boto3Storage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+    default_acl = "public-read"
+
+
+class MediaRootS3Boto3Storage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
+    file_overwrite = False
+{%- elif cookiecutter.cloud_provider == 'GCP' -%}
+from storages.backends.gcloud import GoogleCloudStorage
+
+
+class StaticRootGoogleCloudStorage(GoogleCloudStorage):
+    location = "static"
+    default_acl = "publicRead"
+
+
+class MediaRootGoogleCloudStorage(GoogleCloudStorage):
+    location = "media"
+    file_overwrite = False
+{%- endif %}
