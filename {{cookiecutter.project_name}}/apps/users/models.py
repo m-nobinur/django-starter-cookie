7a40e044f39261a5a7af0ba3db084ae63fdeb219
m-nobinur <mohammadnobinur@gmail.com>
2022-07-27 00:13:05 +0600

    Add custom user model and form

--- {{cookiecutter.project_name}}/apps/users/models.py
+++ {{cookiecutter.project_name}}/apps/users/models.py
@@ -1,23 +1,93 @@
-from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
-from django.urls import reverse
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import User as UserType
+from django.db import models
+from django.db.models.fields import BooleanField, CharField, DateTimeField, EmailField
 from django.utils.translation import gettext_lazy as _
 
 
-class User(AbstractUser):
-    """
-    Default custom user model for {{cookiecutter.project_name}}.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
-    """
+class UserManager(BaseUserManager):
 
-    #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    use_in_migrations = True
 
-    def get_absolute_url(self):
-        """Get url for user's detail view.
-
-        Returns:
-            str: URL for user detail.
+    def _create_user(self, email, password, **extra_fields) -> UserType:
+        """Create and save a user with the given email, and
+        password.
         """
-        return reverse("users:detail", kwargs={"username": self.username})
+        if not email:
+            raise ValueError("The given email must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields) -> UserType:
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields) -> UserType:
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email: EmailField = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    first_name: CharField = models.CharField(
+        _("first name"),
+        max_length=30,
+        blank=True,
+    )
+    last_name: CharField = models.CharField(
+        _("last name"),
+        max_length=150,
+        blank=True,
+    )
+    is_staff: BooleanField = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into " "this admin site."),
+    )
+    is_active: BooleanField = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be "
+            "treated as active. Unselect this instead "
+            "of deleting accounts."
+        ),
+    )
+    is_admin: BooleanField = models.BooleanField(
+        default=False,
+        help_text=_("Designates whether the user is admin or not."),
+    )
+
+    date_joined: DateTimeField = models.DateTimeField(
+        _("date joined"),
+        auto_now_add=True,
+    )
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self) -> str:
+        return self.email
+
+    def has_perm(self, perm, obj=None) -> bool:
+        return True
+
+    def has_module_perms(self, app_label) -> bool:
+        return True
---
+++ {{cookiecutter.project_name}}/apps/users/utils.py
@@ -0,0 +1,24 @@
+name_field_css_classes: str = (
+    "shadow-sm bg-gray-50 border border-gray-300 text-gray-900"
+    " text-sm rounded-lg focus:ring-purple-400 focus:border-purple-400 block w-full p-2.5"
+)
+
+password_field_css_classes: str = (
+    "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm "
+    "rounded-lg focus:ring-purple-400 focus:border-purple-400 block w-full p-2.5"
+)
+
+login_field_css_classes: str = (
+    "px-4 py-2 transition duration-300 border border-gray-300 rounded "
+    "focus:border-transparent focus:outline-none focus:ring-4 focus:ring-purple-200"
+)
+
+login_password_css_classes: str = (
+    "px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent "
+    "focus:outline-none focus:ring-4 focus:ring-purple-200"
+)
+
+remember_me_css_classes: str = (
+    "w-4 h-4 transition duration-300 rounded focus:ring-2 focus:ring-offset-0 "
+    "focus:outline-none focus:ring-purple-200 checked:bg-purple-500"
+)
