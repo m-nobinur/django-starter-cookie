ba79ee8dac37aca616403a60560948268c9df1e6
m-nobinur <mohammadnobinur@gmail.com>
2022-07-26 23:50:47 +0600

    Add editorconfig & update dev configurations

--- {{cookiecutter.project_name}}/manage.py
+++ {{cookiecutter.project_name}}/manage.py
@@ -2,10 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
-def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+def main() -> None:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.local")
 
     try:
         from django.core.management import execute_from_command_line
@@ -19,7 +20,8 @@ def main():
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?")
+                "forget to activate a virtual environment?"
+            )
 
         raise
 
@@ -27,4 +29,7 @@ def main():
 
 
 if __name__ == "__main__":
+    current_path = Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / "apps"))
+
     main()
